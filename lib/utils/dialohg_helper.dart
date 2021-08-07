a31031b37d4e43d8662004832cc55d761cd22e70
Engin <yagmure15@itu.edu.tr>
2021-08-07 21:31:17 +0300

    inform reservation screen is added

--- lib/utils/dialohg_helper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bordatech/utils/show_date_dialog.dart';
-import 'package:flutter/material.dart';
-
-class DialogHelper{
-  static goster(context) => showDialog(context: context, builder: (context) => ShowDateDialog()
-  );
-}
-
